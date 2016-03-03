3f6f1cc768e40028db4ac2afd8bf8d387e62071f
Henrik Nygren <nygrenh@gmail.com>
2016-03-03 23:48:01 +0200

    Initial bitvect attempt

--- src/appriori.rs
+++ src/appriori.rs
@@ -3,25 +3,32 @@ use std::sync::Mutex;
 
 extern crate simple_parallel;
 extern crate num_cpus;
+extern crate bit_vec;
 
 use student::Student;
 use student::Course;
+use bit_vec::BitVec;
 
-pub fn appriori(students: Vec<Student>,
+pub fn appriori(students: Vec<BitVec>,
                  desired_support: f32) {
     println!("Starting appriori with support {:?}", desired_support);
     println!("Generating level 1...");
-    let mut courses: Vec<Vec<&Course>> = students.iter().flat_map(|student| &student.courses ).unique_by(|c| c.code ).map(|course| {
-        let mut inner: Vec<&Course> = Vec::new();
-        inner.push(course);
-        inner
-    }).collect();
-    courses.sort();
+
+    let courses_count = students[0].len();
+    println!("courses_count {:?}", courses_count);
+    let mut courses: Vec<BitVec> = Vec::new();
+    for i in 0..courses_count {
+        let mut vector = BitVec::from_elem(courses_count, false);
+        vector.set(i, true);
+        courses.push(vector)
+    }
+
+    println!("courses: {:?}", courses.len());
 
     println!("First level has been generated!");
     let mut level = 1;
     while !courses.is_empty() {
-        let safe_survivors: Mutex<Vec<Vec<&Course>>> = Mutex::new(Vec::new());
+        let safe_survivors: Mutex<Vec<BitVec>> = Mutex::new(Vec::new());
         let mut pool = simple_parallel::Pool::new(num_cpus::get());
         pool.for_(&courses, |course| {
             let support = calculate_support(&students, &course);
@@ -38,42 +45,44 @@ pub fn appriori(students: Vec<Student>,
         survivors.sort();
         courses = generate(&survivors);
         println!("Generated {:?} candidates", courses.len());
-        courses = prune(courses, &survivors);
-        println!("{:?} candidates survived prune", courses.len());
+        // courses = prune(courses, &survivors);
+        // println!("{:?} candidates survived prune", courses.len());
         println!("Starting to generate level {:?} candidates...", level);
     }
 }
 
-pub fn prune<'a>(courses: Vec<Vec<&'a Course>>, prev: &Vec<Vec<&'a Course>>) -> Vec<Vec<&'a Course<'a>>> {
-    let mut res = Vec::new();
-    for course in courses {
-        let mut all = true;
-        for i in 0..(course.len()) {
-            let mut test = course.clone();
-            test.remove(i);
-            if !prev.contains(&test) {
-                all = false;
-                break;
-            }
-        }
-        if all {
-            res.push(course);
-        }
-    }
-    res
-}
-
-pub fn calculate_support(students: &[Student], courses: &[&Course]) -> f32 {
+// pub fn prune<'a>(courses: Vec<Vec<&'a Course>>, prev: &Vec<Vec<&'a Course>>) -> Vec<Vec<&'a Course<'a>>> {
+//     let mut res = Vec::new();
+//     for course in courses {
+//         let mut all = true;
+//         for i in 0..(course.len()) {
+//             let mut test = course.clone();
+//             test.remove(i);
+//             if !prev.contains(&test) {
+//                 all = false;
+//                 break;
+//             }
+//         }
+//         if all {
+//             res.push(course);
+//         }
+//     }
+//     res
+// }
+//
+pub fn calculate_support(students: &Vec<BitVec>, courses: &BitVec) -> f32 {
     let mut count = 0;
-    for student in students {
-        if courses.iter().all(|course| student.course_codes.contains(&course.code) ) {
+    for student_vector in students {
+        let mut diff = courses.clone();
+        diff.difference(student_vector);
+        if diff.none() {
             count += 1
         }
     }
     (count as f32 / students.len() as f32) as f32
 }
 
-pub fn generate<'a>(courses: &Vec<Vec<&'a Course>>) -> Vec<Vec<&'a Course<'a>>> {
+pub fn generate<'a>(courses: &Vec<BitVec>) -> Vec<BitVec> {
     let mut res = Vec::new();
     if courses.len() <= 1 {
         return res;
@@ -86,8 +95,10 @@ pub fn generate<'a>(courses: &Vec<Vec<&'a Course>>) -> Vec<Vec<&'a Course<'a>>> 
         while index2 < courses.len() {
             let second =  &courses[index2];
             if !adding_makes_sense(first, second) {
+                // println!("Didn't make sense: {:?} and {:?}", first, second);
                 break;
             }
+
             res.push(union(first, second));
             index2 += 1;
         }
@@ -96,21 +107,27 @@ pub fn generate<'a>(courses: &Vec<Vec<&'a Course>>) -> Vec<Vec<&'a Course<'a>>> 
     res
 }
 
-pub fn adding_makes_sense<'a>(first: &[&Course<'a>], second: &[&Course<'a>]) -> bool {
-    if (first.len() != second.len()) || first.is_empty() {
-        return false;
+pub fn adding_makes_sense<'a>(first: &BitVec, second: &BitVec) -> bool {
+    // TODO: Do we need this?
+    // if (first.len() != second.len()) || first.is_empty() {
+    //     return false;
+    // }
+    let mut count = 0;
+    let limit = first.iter().filter(|x| *x).count() - 1;
+    for i in 0..(first.len() - 1) {
+        if count >= limit {
+            break;
+        }
+        if first.get(i).unwrap() != second.get(i).unwrap() {
+            return false;
+        }
+        count += 1;
     }
-    let end = first.len() - 1;
-    first[0..end] == second[0..end]
-
+    true
 }
 
-pub fn union<'a>(first: &Vec<&'a Course<'a>>, second: &[&'a Course<'a>]) -> Vec<&'a Course<'a>> {
-    let mut res = first.clone();
-    for e in second {
-        if !res.contains(e) {
-            res.push(e);
-        }
-    }
-    res
+pub fn union<'a>(first: &BitVec, second: &BitVec) -> BitVec {
+    let mut result = first.clone();
+    result.union(second);
+    result
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,41 @@
 #![feature(plugin)]
 // #![plugin(clippy)]
 #[macro_use] extern crate itertools;
+extern crate bit_vec;
 
 mod student;
 mod appriori;
 
 use student::Student;
+use itertools::Itertools;
+use std::collections::HashMap;
+
+use bit_vec::BitVec;
 
 fn main() {
     let data = include_str!("data-2016.csv");
     let students = Student::create(data);
-    appriori::appriori(students, 0.04);
+    let mut courses: Vec<u32> = students.iter().flat_map(|student| &student.courses ).unique_by(|c| c.code ).map(|course| {
+        course.code
+    }).collect();
+    courses.sort();
+    let mut courses_to_ids = HashMap::new();
+    let mut ids_to_courses = HashMap::new();
+    for (index, course) in courses.iter().enumerate() {
+        &courses_to_ids.insert(course, index);
+        &ids_to_courses.insert(index, course);
+    };
+
+    let vector_students: Vec<BitVec> = students.iter().map(|student| {
+        let codes: Vec<usize> = student.course_codes.iter().map(|c| *courses_to_ids.get(c).unwrap()).collect();
+        let mut vector = BitVec::from_elem(courses.len(), false);
+
+        for id in &codes {
+            vector.set(*id, true);
+        }
+
+        vector
+    }).collect();
+    println!("Vectorized students!");
+    appriori::appriori(vector_students, 0.04);
 }
